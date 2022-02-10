94f26f2789d380bbd2eda0cac1708349bf833f6f
rmoquete36 <98977363+rmoquete36@users.noreply.github.com>
2022-02-09 21:18:30 -0500

    Final Assignment 2

--- js/scripts.js
+++ js/scripts.js
@@ -1,28 +1,4 @@
-// $('#Beacon').on('click', function() {
-//   $(".right-half").html("<img src='https://triptins.com/wp-content/uploads/2016/03/Breakneck-Ridge-Hike-View.jpeg'/>");
-//  	$('.right-half').append('1 hour and 20 minutes from NYC <p> A 3.7 mile loop offering beautiful views of the Hudson River </p> ');
-// })
-// $('#Catskill').on('click', function() {
-//   $(".right-half").html("<img src='https://imaginelovinglife.co/hiking%20images/nynj/sams%20point/_sams%20point.jpg'/>");
-//   $('.right-half').append('2 hours from NYC <p> A 8.3 mile loop on the highest point of the Shawangunk Ridge </p> ');
-// })
-//
-// $('#TomkinsCove').on('click', function() {
-//   $(".right-half").html("<img src='https://appalachiantrail.org/wp-content/uploads/2019/02/Warwick-1-John-DeSanto-840x560.jpg'/>");
-//   $('.right-half').append('1 hour from NYC <p> This trail stretches 88 miles in NY alone! </p>');
-// })
-//
-// $('#Ithaca').on('click', function() {
-//   $(".right-half").html("<img src='https://myhikes.org/images_uploads/Buttermilk%20Falls%20NY%2FIMG_3683_20170527234649UTC_small.JPG'/>");
-//   $('.right-half').append('4 hours and 20 minutes from NYC <p> A 1.6 mile loop featuring a waterfall </p>');
-// })
-//
-// $('#LakePlacid').on('click', function() {
-//   $(".right-half").html("<img src='https://cdn.securem2.com/commonimages/business/11114/2011/10/19527-11114-111026141939-17026-display3.jpg'/>");
-//   $('.right-half').append('5 hours from NYC <p> A 4.4 mile loop offering views of Lake Placid ');
-// })
-
-
+// coding is awesome!
 $('.trail-container').on('click', function() {
   var trailPhoto = $(this).data('trail-photo')
   $(".right-half").html(`<img src='${trailPhoto}'/>`);
